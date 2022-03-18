3ebf4e1d165afe9295e3e2613352e861c0f5882b
Gerson Azabache Martinez <gerson_dunk@hotmail.com>
2022-03-18 03:08:52 -0500

    initial commit

---
+++ MyAlgorithmsCollection/Algorithms/BinarySearch.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmsBook.Algorithms
+{
+    public class BinarySearch
+    {
+        public static int Run(int[] arreglo, int x)
+        {       
+            int left = 0;
+            int right = arreglo.Length - 1;
+
+            while(left <= right)
+            {
+                int middle = left + (right-left) / 2;
+
+                if (arreglo[middle] == x)
+                    return middle;
+
+                if (x > arreglo[middle])
+                    left = middle + 1;
+                else
+                    right = middle - 1;
+            }
+            return -1;
+        }
+    }
+}
---
+++ MyAlgorithmsCollection/Program.cs
@@ -0,0 +1,13 @@
+﻿using AlgorithmsBook.Algorithms;
+using System;
+
+namespace AlgorithmsBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(BinarySearch.Run(new int[] { 1,2,3,4,5,6,7,8,9 },90));
+        }
+    }
+}
