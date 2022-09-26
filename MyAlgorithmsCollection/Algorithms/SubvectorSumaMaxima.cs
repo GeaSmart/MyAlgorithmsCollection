1c23b55274a5e2d10f65b975946b6fdcda833cb8
Gerson Azabache <gerson_dunk@hotmail.com>
2022-09-25 23:08:15 -0500

    subvector de suma maxima added

---
+++ MyAlgorithmsCollection/Algorithms/SubvectorSumaMaxima.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyAlgorithmsCollection.Algorithms
+{
+    class SubvectorSumaMaxima
+    {
+        public static int SumaMaxima(int[] vector)
+        {
+            int suma = 0;
+            int maxSum = 0;
+
+            for (int i = 0; i < vector.Length; i++)
+            {
+                for (int j = i; j < vector.Length; j++)
+                {
+                    suma += vector[j];
+
+                    if (suma > maxSum)
+                        maxSum = suma;
+                }
+                suma = 0;
+            }
+            return maxSum;
+        }
+    }
+}
--- MyAlgorithmsCollection/Program.cs
+++ MyAlgorithmsCollection/Program.cs
@@ -8,8 +8,8 @@ namespace AlgorithmsBook
     {
         static void Main(string[] args)
         {
-            BubbleSort.Run(new int[] {3,7,6,10,1,3,20,5,4,33,3,7,7,56,2 });
-            //Console.WriteLine();
+            int[] lista = new int[] { 1, 2, -4, 3, 3, -1, 2 };
+            Console.WriteLine(SubvectorSumaMaxima.SumaMaxima(lista));
         }
     }
 }
