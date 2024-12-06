16eb15798bc41204b6034be8dd17590e6a9a3efd
Gerson Azabache <gerson_dunk@hotmail.com>
2024-12-06 01:09:32 -0500

    reverse linked list

---
+++ MyAlgorithmsCollection/Algorithms/ReverseLinkedList.cs
@@ -0,0 +1,33 @@
+﻿namespace MyAlgorithmsCollection.Algorithms
+{
+    public class ReverseLinkedList
+    {
+        public static Node Reverselist(Node nodo)
+        {
+            var current = nodo;
+            Node prev = null;
+
+            while (current != null)
+            {
+                var next = current.Next;
+                current.Next = prev;
+                prev = current;
+                current = next;
+            }
+            return prev;
+        }
+    }
+
+    // Clase para almacenar nodos
+    public class Node
+    {
+        public Node Next;
+        public int Data;
+
+        public Node(int data)
+        {
+            Data = data;
+            Next = null;
+        }
+    }
+}
--- MyAlgorithmsCollection/Program.cs
+++ MyAlgorithmsCollection/Program.cs
@@ -8,8 +8,23 @@ namespace AlgorithmsBook
     {
         static void Main(string[] args)
         {
-            var text = "hola, loco mundo esto es hola hola genial. Aunque loco loco loco";
-            MostRepeatedWord.Run(text);
+            // Crear lista enlazada 1 -> 2 -> 3 -> 4 -> 5
+            Node mi_nodo = new Node(1);
+            mi_nodo.Next = new Node(2);
+            mi_nodo.Next.Next = new Node(3);
+            mi_nodo.Next.Next.Next = new Node(4);
+            mi_nodo.Next.Next.Next.Next = new Node(5);
+
+            // Invierto la lista enlazada
+            mi_nodo = ReverseLinkedList.Reverselist(mi_nodo);
+
+            // Imprimir
+            while (mi_nodo != null)
+            {
+                Console.WriteLine(mi_nodo.Data);
+                mi_nodo = mi_nodo.Next;
+            }
+            Console.WriteLine();
         }
     }
 }
